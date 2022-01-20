3095579b79d61aa6cbb815e31e48cc3a4d65d7e4
David Jones <davidthomjones@gmail.com>
2022-01-21 08:56:29 +1000

    Another tweak?

--- release/canvas-collections.user.js
+++ release/canvas-collections.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         canvas-collections
 // @namespace    https://djon.es/
-// @version      0.2.2
+// @version      0.2.3
 // @description  Modify Canvas LMS modules to support collections of modules and their representation
 // @author       David Jones
 // @match        https://*/courses/*
