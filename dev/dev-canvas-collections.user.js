a235ed735dba7f1f2dc776d528ff5fc0837bfb62
David Jones <davidthomjones@gmail.com>
2022-06-02 06:49:36 +1000

    Going for .8 release

--- dev/dev-canvas-collections.user.js
+++ dev/dev-canvas-collections.user.js
@@ -10,5 +10,5 @@
 // @license      ISC
 // @homepage     https://github.com
 // @require      https://unpkg.com/circular-progress-bar
-// @require      file://C:\Users\s2986288\code\canvas-collections\dev\canvas-collections.user.js
+// @require      file://e:\migration\code\canvas-collections\dev\canvas-collections.user.js
 // ==/UserScript==
