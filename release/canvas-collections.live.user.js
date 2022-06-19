3ccf97c6596d71976dae169fb3633e2a9487dee2
David Jones <davidthomjones@gmail.com>
2022-06-20 07:00:49 +1000

    Update live userscript 0.8.3

--- release/canvas-collections.live.user.js
+++ release/canvas-collections.live.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         canvas-collections
 // @namespace    https://djon.es/
-// @version      0.8.2
+// @version      0.8.3
 // @description  Modify Canvas LMS modules to support collections of modules and their representation
 // @author       David Jones
 // @match        https://*/courses/*
@@ -9,6 +9,5 @@
 // @source       https://github.com/djplaner/canvas-collections.git
 // @license      ISC
 // @homepage     https://github.com
-// @require      https://rawcdn.githack.com/djplaner/canvas-collections/215f296cafd8f54274c07cb74c6935f92b43a50b/release/canvas-collections.min.js
-// @require      https://rawcdn.githack.com/djplaner/canvas-collections/7bab9d7a57c3f642df82cd2b69b112748ba2d8aa/release/canvas-collections.min.js
+// @require      https://rawcdn.githack.com/djplaner/canvas-collections/6a0b53fe96d85771f8eb247c53f03da1f361a9bd/release/canvas-collections.min.js
 // ==/UserScript==
