71f1bf51bf744ceb7505530709cfecd3529b45a1
David Jones <davidthomjones@gmail.com>
2023-01-14 16:58:18 +1000

    meta data just about finished #161

--- src/components/Configuration/ModuleMetaDataConfiguration.svelte
+++ src/components/Configuration/ModuleMetaDataConfiguration.svelte
@@ -44,6 +44,31 @@
       return;
     }
 
+    // do some sanitisation of the HTML https://github.com/apostrophecms/sanitize-html
+    // - value will be allowed HTML, including iframes
+    // - name is not allowed HTML
+    const sanitisedName = sanitize(name.value, false);
+    const sanitisedValue = sanitize(value.value);
+
+    // if the sanitised name is different to the original, warn the user
+    // also warn if the sanitised name is empty
+    if (sanitisedName !== name.value || sanitisedName === "") {
+      if (
+        !confirm(
+          `The new metadata item name 
+    ${name.value} 
+has been sanitised (removing forbidden characters) to 
+    ${sanitisedName}. 
+Please check that this is what you want. (If the sanitised name is empty, the metadata item will not be added.)`
+        )
+      ) {
+        return;
+      }
+      if (sanitisedName === "") {
+        return;
+      }
+    }
+
     // check for duplicate of existing name
     if (
       $collectionsStore["MODULES"][moduleId].metadata.hasOwnProperty([
@@ -56,10 +81,7 @@
       return;
     }
 
-    // do some sanitisation of the HTML https://github.com/apostrophecms/sanitize-html
-    const sanitisedValue = sanitizeValue(value.value);
-
-    $collectionsStore["MODULES"][moduleId].metadata[name.value] =
+    $collectionsStore["MODULES"][moduleId].metadata[sanitisedName] =
       sanitisedValue;
     $configStore["needToSaveCollections"] = true;
 
@@ -71,18 +93,25 @@
   }
 
   /**
-   * @function sanitizeValue
+   * @function sanitize
    * @param {string} value - the value to be sanitised
+   * @param {boolean} allowHtml - whether to allow HTML or not
    * @returns {string} - the sanitised value
    * @description Allow HTML, including iframes but do other sanity checks
    */
-  function sanitizeValue(value: string) {
+  function sanitize(value: string, allowHtml: boolean = true) {
     let allowedTags = sanitizeHtml.defaults.allowedTags;
     let allowedAttributes = {};
     allowedTags = allowedTags.concat("iframe");
     allowedAttributes = {
       iframe: ["src", "width", "height", "frameborder", "allowfullscreen"],
     };
+
+    if (!allowHtml) {
+      allowedTags = [];
+      allowedAttributes = {};
+    }
+
     return sanitizeHtml(value, {
       allowedTags: allowedTags,
       allowedAttributes: allowedAttributes,
@@ -128,23 +157,68 @@ for the module
     ) {
       return;
     }
+    // create a complete new object to remove the key
+    // Necessary to ensure the reactive display of the list of metadata
+    // removes the correct entry (it doesn't if you just delete from the existing object)
     let metadata = $collectionsStore["MODULES"][moduleId].metadata;
     delete metadata[key];
-    debug(metadata);
 
+    // force the reactive update of the entire list of metadata
     $collectionsStore["MODULES"][moduleId].metadata = metadata;
-    debug(`global metadata`);
-    debug($collectionsStore["MODULES"][moduleId].metadata);
     $configStore["needToSaveCollections"] = true;
   }
+
+  /**
+   * @function changeMetaDataName
+   * @param {String} originalName - the original name of the metadata entry
+   * @description User has released a key to change the name field of a metadata entry
+   * - make a copy of the metadata object
+   * - add the new entry as a copy from the original entry
+   * - delete the original entry
+   * - reassign the original metadata object to the copy
+   */
+  function changeMetaDataName(originalName: string) {
+    const name = document.getElementById(
+      `cc-module-config-${moduleId}-metadata-${originalName}-name`
+    ) as HTMLInputElement;
+
+    let metadata = $collectionsStore["MODULES"][moduleId].metadata;
+    metadata[name.value] = metadata[originalName];
+    delete metadata[originalName];
+    $collectionsStore["MODULES"][moduleId].metadata = metadata;
+    $configStore["needToSaveCollections"] = true;
+  }
+
+  //------------- HELP tooltips and urls
+  const HELP = {
+    name: {
+      tooltip: `<p>Each metadata value is given a name. Only certain characters can be used. No HTML tags allowed.<p>
+			<p>Changes will only take effect when you move focus away from the name field.</p>`,
+    },
+    value: {
+      tooltip: `<p>The value for the metadata element. HTML tags are allowed.</p>`,
+    },
+  };
 </script>
 
 <div class="cc-module-config-additional-metadata border border-trbl">
   <table>
     <thead>
       <tr>
-        <th>Name</th>
-        <th>Value</th>
+        <th
+          >Name
+          <sl-tooltip>
+            <div slot="content">{@html HELP.name.tooltip}</div>
+            <i class="icon-question cc-module-icon" />
+          </sl-tooltip>
+        </th>
+        <th
+          >Value
+          <sl-tooltip>
+            <div slot="content">{@html HELP.value.tooltip}</div>
+            <i class="icon-question cc-module-icon" />
+          </sl-tooltip>
+        </th>
         <th>Action</th>
       </tr>
     </thead>
@@ -154,7 +228,7 @@ for the module
           ><input
             type="text"
             id="cc-module-config-{moduleId}-metadata-add-name"
-            pattern={String.raw`[^"]`}
+            pattern={String.raw`[^"<>]`}
             on:keydown|stopPropagation
             on:keyup={() => (readyToAdd = isReadyToAdd())}
           /></td
@@ -184,6 +258,7 @@ for the module
               type="text"
               id="cc-module-config-{moduleId}-metadata-{key}-name"
               on:keydown|stopPropagation
+              on:focusout={changeMetaDataName(key)}
               value={key}
               pattern={String.raw`[^"]`}
             />
@@ -195,7 +270,6 @@ for the module
               bind:value={$collectionsStore["MODULES"][moduleId].metadata[key]}
               on:click={() => ($configStore["needToSaveCollections"] = true)}
               on:keydown|stopPropagation
-              on:keyup={() => ($configStore["needToSaveCollections"] = true)}
             />
           </td>
           <td>
@@ -232,5 +306,10 @@ for the module
   td {
     padding: 0.5rem;
     margin: 0.5rem;
+  }
+
+  sl-tooltip {
+    text-align: left;
+    white-space: normal;
   }
 </style>
