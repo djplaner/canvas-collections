7bb31d0e852e09aa1a8b494339ff78bc820dbfba
David Jones <davidthomjones@gmail.com>
2023-01-19 07:25:30 +1000

    Ensuring userscript is live updating

--- dev/canvas-collections.user.js
+++ dev/canvas-collections.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         canvas-collections
 // @namespace    https://djon.es/
-// @version      0.9.9
+// @version      0.9.10
 // @description  Modify Canvas LMS modules to support collections of modules and their representation
 // @author       David Jones
 // @match        https://*/courses/*
@@ -9,6 +9,8 @@
 // @source       https://github.com/djplaner/canvas-collections.git
 // @license      ISC
 // @homepage     https://github.com/djplaner/canvas-collections/tree/main#canvas-collections
+// @updateURL    https://raw.githubusercontent.com/djplaner/canvas-collections/main/dev/canvas-collections.user.js
+// @downloadURL  https://raw.githubusercontent.com/djplaner/canvas-collections/main/dev/canvas-collections.user.js
 // @require      https://unpkg.com/circular-progress-bar
 // @require      https://rawcdn.githack.com/djplaner/canvas-collections/62a4248058d13d32c574f0b620760891651587a7/src/juice/juice_client.js
 // @require      https://unpkg.com/@lamplightdev/aeon
