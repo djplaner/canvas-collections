39423fbae53fcc99fd1e702105ab17fb27c8aa3c
David Jones <davidthomjones@gmail.com>
2023-01-26 09:11:55 +1000

    Shoelace v2 working closes #183
    closes #183

--- src/CanvasCollections.svelte
+++ src/CanvasCollections.svelte
@@ -278,11 +278,11 @@
 <svelte:head>
   <link
     rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.88/dist/themes/light.css"
+    href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0/dist/themes/light.css"
   />
   <script
     type="module"
-    src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.88/dist/shoelace.js"
+    src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0/dist/shoelace.js"
   ></script>
 </svelte:head>
 
