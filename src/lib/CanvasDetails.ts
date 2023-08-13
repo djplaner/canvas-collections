e5cf3c3eb8bd61936d4c3ea92ec6c773c831e642
David Jones <davidthomjones@gmail.com>
2023-08-13 13:44:49 +1000

    Added "completiion_summary" to courseModules #8

--- src/lib/CanvasDetails.ts
+++ src/lib/CanvasDetails.ts
@@ -19,13 +19,23 @@
  * @class CanvasDetails
  * @description Given a course Id retrieve the info Collections requires
  * about the course and its modules via Canvas API calls
+ * 
+ * Currently it's done via sequential calls to the following Canvas API calls
+ * - requestCourseObject
+ * - requestModuleContents
+ * 
+ * refreshCanvasDetails also available to refresh everything every once and a while
+ * 
+ * TODO
+ * - Probably more efficient to do these in parallel
+ * - Add requestModuleCompletion (iff only current user is a student) to add completion
+ *   But a challenge here will be how keep this up to date with student progress
  */
 
 import { configStore } from "../stores";
 import { get } from "svelte/store";
 
 import { wf_fetchData } from "./CanvasSetup";
-//import UniversityDateCalendar from "./university-date-calendar";
 
 export class CanvasDetails {
   public courseObject: object;
@@ -33,12 +43,14 @@ export class CanvasDetails {
   public studyPeriod: string;
   public courseCode: string;
 
+  private courseModuleItems: object;
 
   private config: object;
   //  private csrfToken: string;
   private configStore = get(configStore);
   private currentHostName: string;
   private baseApiUrl = this.configStore["baseApiUrl"];
+  private isStudent = !this.configStore["editMode"];
   //  private courseId: number;
   private finishedCallBack: Function;
   //  private calendar: UniversityDateCalendar;
@@ -60,7 +72,7 @@ export class CanvasDetails {
     //    this.courseId = this.CONFIG.courseId;
 
     this.currentHostName = document.location.hostname;
-//    this.baseApiUrl = this.configStore["baseApiUrl"] //`https://${this.currentHostName}/api/v1`;
+    //    this.baseApiUrl = this.configStore["baseApiUrl"] //`https://${this.currentHostName}/api/v1`;
     // convert courseId to integer
     this["config"]["courseId"] = parseInt(this["config"]["courseId"]);
 
@@ -87,107 +99,11 @@ export class CanvasDetails {
         if (msg.status === 200) {
           this.courseObject = msg.body;
           //this.generateSTRM()
-          this.requestModuleInformation();
+          this.requestModulesItems();
         }
       }
     );
   }
-
-  /**
-   * @descr Examine Canvas course object's course_code attribute in an attempt
-   * to extract the STRM and subsequently calculate the year, period and
-   * other data
-   *
-   * Production sites:
-   *    Organisational Communication (COM31_2226)
-   *
-   * DEV sites:
-   *    DEV_2515LHS_3228
-   *
-   * ORG sites:
-   *     AEL_SHOW1
-   *
-   * TODO rejig based on scapeLib/parseCourseInstanceId (ael-automation)
-   * In particular to handle the "YP" course ids
-   */
-
-/*  generateSTRM() {
-    if (!this.hasOwnProperty("calendar")) {
-//      this.calendar = new UniversityDateCalendar();
-    }
-
-    // TODO this is where we might check if there is an existing default
-    // study period already set and thus bypass getCurrentPeriod
-
-    // we pass course_code to calendar because it's the main object that is
-    // available to all users. the sis_course_id might be better but students
-    // don't see it
-    this.studyPeriod = this.calendar.getCurrentPeriod(
-      this.courseObject.course_code
-    )
-//    const success = this.calendar.setStudyPeriod(this.studyPeriod)
-    this.strm = this.calendar.getStudyPeriod()
-    // aboutStudyPeriod is an object with human readable information about the
-    // study period - typically strings for
-    // - year - full year
-    // - period - descriptive name for the period
-    // - type - string specifying the type of study period
-    //		this.aboutStudyPeriod = this.calendar.parseStudyPeriod(this.studyPeriod);
-
-    this.parseStrm()
-  }
-*/
-  /**
-   * @descr Parse the STRM and set the type, year, period
-   * Based on Griffith STRM definition
-   * https://intranet.secure.griffith.edu.au/computing/using-learning-at-griffith/staff/administration/course-ID
-   */
-
-/*  parseStrm() {
-    this.type = undefined;
-    this.year = undefined;
-    this.period = undefined;
-
-    // return if this.strm undefined
-    if (this.strm === undefined) {
-      return;
-    }
-
-    // break up this.strm into individual characters
-    const strm = this.strm.split("");
-
-    // if more than four chars then return
-    if (strm.length > 4) {
-      console.error(`cc_Controller: parseStrm: strm too long: ${this.strm}`);
-      return;
-    }
-
-    // check all chars are numeric
-    for (let i = 0; i < strm.length; i++) {
-      if (isNaN(strm[i])) {
-        console.error(
-          `cc_Controller: parseStrm: strm not numeric: ${this.strm}`
-        );
-        return;
-      }
-    }
-
-    this.type = strm[0];
-    // this.year is the middle two characters prepended by 20
-    this.year = `20${strm[1]}${strm[2]}`;
-    // this.period (initially) is that last char
-    this.period = strm[3];
-
-    // period value needs translation based on type
-
-    // default is Griffith trimester
-    let translate = { 1: 1, 5: 2, 8: 3 };
-    if (this.type === 2) {
-      translate = { 1: 1, 2: 1, 3: 2, 4: 2, 5: 3, 6: 3, 7: 4 };
-    }
-    this.period = translate[this.period];
-  }
-  */
 
   /**
    * @function requestModuleInformation
@@ -197,17 +113,98 @@ export class CanvasDetails {
    *    add &student_id=<studentCanvasId>
    */
 
-  requestModuleInformation() {
+  requestModulesItems() {
     wf_fetchData(
-      `${this.baseApiUrl}/courses/${this.config.courseId}/modules?include=content_details`
+      `${this.baseApiUrl}/courses/${this.config.courseId}/modules?include=items&per_page=500`
       //`${this.baseApiUrl}/courses/${this.config.courseId}/modules?include=items&per_page=500&student_id=23854443`
     ).then((msg) => {
       if (msg.status === 200) {
         this.courseModules = msg.body;
-        this.finishedCallBack();
+        this.requestModulesCompletion();
       }
     });
   }
+
+  /**
+   * @function requestModuleCompletionInformation
+   * @descr If the user is a student then request the module completion information
+   */
+
+  requestModulesCompletion() {
+    const studentId = '23854443';
+    //if (this.isStudent) {
+    if (true) {
+      wf_fetchData(
+        `${this.baseApiUrl}/courses/${this.config.courseId}/modules?include=items&per_page=500&student_id=${studentId}`
+      ).then((msg) => {
+        if (msg.status === 200) {
+          this.courseModuleItems = msg.body;
+          this.summariseCompletion();
+          this.finishedCallBack();
+        }
+      });
+    } else {
+      this.finishedCallBack();
+    }
+  }
+
+  /**
+   * @function summariseCompletion
+   * @descr For each module item in this.courseModules add a propery complection_summary
+   *    {
+   *      "completion_count": <number of items with completion requirements>,
+   *      "completed_count": <number of items with completion requirements completed>,
+   *    }
+   * Based on data from this.courseModuleItems
+   * 
+   * Complication during testing as staff member with hard coded student
+   * - staff member can see all unpublished modules
+   * - student can't so courseModulesItems array isn't the same as courseModules
+   *   (will be for a student)
+   * - hence need to create a hash for both arrays keyed on the id property
+   */
+
+  summariseCompletion() {
+    // create necessary work around hashes
+    const courseModulesHash = {};
+    this.courseModules.forEach((module) => {
+      courseModulesHash[module["id"]] = module;
+    });
+    const courseModuleItemsHash = {};
+    this.courseModuleItems.forEach((module) => {
+      courseModuleItemsHash[module["id"]] = module;
+    });
+
+    // loop through the courseModulesHash
+    for (const key in courseModulesHash) {
+      // initialise the completion_summary object
+      courseModulesHash[key]["completion_summary"] = {
+        completion_count: 0,
+        completed_count: 0,
+      };
+
+      // skip to next if the module doesn't have any items
+      if (!courseModuleItemsHash.hasOwnProperty(key)) {
+        continue;
+      }
+
+      // loop through each item in the module
+      courseModuleItemsHash[key]["items"].forEach((item) => {
+        // if the item has a completion requirement property
+        if (item.hasOwnProperty("completion_requirement")) {
+          courseModulesHash[key]["completion_summary"]["completion_count"]++;
+          // if the item is completed
+          if (item["completion_requirement"]["completed"]) {
+            // increment the completed_count
+            courseModulesHash[key]["completion_summary"]["completed_count"]++;
+          }
+        }
+      });
+    }
+
+
+  }
+
 }
 
 
