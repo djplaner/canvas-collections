a149fbfbc6944f2b77b856c150f8dd618e691d2c
David Jones <davidthomjones@gmail.com>
2023-08-16 21:58:13 +1000

    Update CollectionsNavigation.svelte
    #78

--- src/components/CollectionsNavigation.svelte
+++ src/components/CollectionsNavigation.svelte
@@ -61,7 +61,7 @@
   <ul>
     {#each collectionNames as collectionName, i}
       {#if !($collectionsStore["COLLECTIONS"][collectionName].hide && !$configStore["editMode"])}
-        <li class="cc-nav {activeCollection[collectionName]}">
+        <li class="cc-nav-collection {activeCollection[collectionName]}">
           <a
             href="#cc-collection-{i}"
             on:click|stopPropagation={() => navigateCollections(collectionName)}
@@ -73,10 +73,38 @@
         </li>
       {/if}
     {/each}
+    <li class="cc-nav-menu">
+      <a href="javscript:void(0);"> ☰ </a>
+    </li>
   </ul>
 </div>
 
 <style>
+  @media (max-width: 600px) {
+    .cc-nav-collection {
+      display: none ! important;
+    }
+    .cc-nav-menu {
+      display: block ! important;
+    }
+
+    .cc-nav {
+      width: 20%;
+    }
+  }
+
+  @media (min-width: 600px) {
+    .cc-nav-collection {
+      display: block;
+    }
+    .cc-nav-menu {
+      display: none ! important;
+    }
+    .cc-nav {
+      width: 100%;
+    }
+  }
+
   .cc-content {
     clear: both;
   }
@@ -92,14 +120,14 @@
     text-align: justify;
     justify-items: stretch;
     grid-gap: 0.1rem;
-    grid-template-columns: repeat(auto-fit, minmax(min(10rem,100%), 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(min(10rem, 100%), 1fr));
     list-style-type: none;
     margin: 0;
     padding: 0;
     overflow: hidden;
     background-color: #eee;
     width: 100%;
-/*    display: table;
+    /*    display: table;
     table-layout: fixed; */
   }
 
@@ -121,11 +149,29 @@
   .cc-nav ul li {
     display: block;
     width: 100%;
-    /*   border-right: 1px solid #000; */
-   /* float: none; */
   }
 
-  li.cc-nav a {
+  .cc-nav ul li {
+    display: block;
+    width: 100%;
+    /*   border-right: 1px solid #000; */
+    /* float: none; */
+  }
+
+  li.cc-nav-menu a {
+    display: block;
+    font-size: 2rem;
+    text-align: right !important;
+    text-decoration: none;
+    color: #2d3b45;
+    padding: 1em 0.8em !important;
+    border-top: 4px solid #eee;
+    box-sizing: border-box;
+    font-size: 1.2em;
+    transition: background 0.3s linear 0s !important;
+  }
+
+  li.cc-nav-collection a {
     display: block;
     text-align: center !important;
     text-decoration: none;
